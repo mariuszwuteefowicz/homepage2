0e9ef45de77fb9764ef34c901266cf080a2a059a
Mariusz <mariusz.c008@protonmail.com>
2023-10-19 18:44:19 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-const firstName = 'Maciek';
+const firstName = 'Mariusz';
 const age = 35;
 
 console.log(firstName);
 console.log(age);
 
 console.log(`Nazywam sie ${firstName} i mam ${age} lat.`);
+
+////////////////////////
+
+const header = document.querySelector('.page-header__heading--js')
+
+console.log (header)    //wyswietla h1
+
+console.log (header.innerHTML)  //wyswietla tylko tekst
+
+//header.innerHTML = 'Hej!';  //modyfikacja tresci
+
+console.log (header.innerHTML)
+
+//header.innerHTML = `Nazywam sie ${firstName} i mam ${age} lat.`;    //modyfikacja tresci
+
+console.log (header.innerHTML)
+
+console.log (header.style)  //mozliwe modyfikacje styli
+
+//header.style.color = 'red';     //modyfikacja koloru
+
+////////////////////////homework
+
+const contentJs = document.querySelector('.my-article__paragraph--js');
+contentJs.innerHTML = 'jestem podmieniony JS 🤯';
