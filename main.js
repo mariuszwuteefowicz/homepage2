395ab37cb1887f424c47d95d30d003ad582053d6
Mariusz <mariusz.c008@protonmail.com>
2024-04-02 10:29:05 +0200

    js modification

--- main.js
+++ main.js
@@ -1,31 +1,6 @@
-const firstName = 'Mariusz';
-const age = 35;
-
-console.log(firstName);
-console.log(age);
-
-console.log(`Nazywam sie ${firstName} i mam ${age} lat.`);
-
-////////////////////////
-
-const header = document.querySelector('.page-header__heading--js')
-
-console.log (header)    //wyswietla h1
-
-console.log (header.innerHTML)  //wyswietla tylko tekst
-
-//header.innerHTML = 'Hej!';  //modyfikacja tresci
-
-console.log (header.innerHTML)
-
-//header.innerHTML = `Nazywam sie ${firstName} i mam ${age} lat.`;    //modyfikacja tresci
-
-console.log (header.innerHTML)
-
-console.log (header.style)  //mozliwe modyfikacje styli
-
-//header.style.color = 'red';     //modyfikacja koloru
-
-////////////////////////homework4
+///////////        homework4
+//
+// podmiana akpitu za pomoca JS
+//
 //const contentJs = document.querySelector('.my-article__paragraph--js');
-//contentJs.innerHTML = 'jestem podmieniony JS 🤯';
+//contentJs.innerHTML = 'jestem podmieniony JS !!! 🤯';
