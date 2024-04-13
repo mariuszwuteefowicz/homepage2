823fda81379e0dfd9ee9819fc2be22f7254ae5ae
Mariusz <mariusz.c008@protonmail.com>
2024-04-13 16:33:39 +0200

    add background button

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,10 @@ button.addEventListener('click', (e) => {
     const header = document.querySelector('.page-header__heading--js');
     header.innerHTML = 'Proszę nie hakować mojej strony 🤯😄👌';
 });
+
+
+const buttonBackground = document.querySelector('.page-header__buttonBackground--js');
+
+buttonBackground.addEventListener('click', (e) => {
+    document.documentElement.style.setProperty('--backgroundColor', 'rgb(131, 225, 255')
+});
